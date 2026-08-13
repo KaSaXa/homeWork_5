a83e799bc428ca5721ec6c640bdc4db412219da1
KaSaXa <akurmelevv@gmail.com>
2026-08-14 00:20:58 +0300

    dasCompled

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-// Попытка создать пул реквест
+
 // task 1
         int clientOS = 0; // 0 - iOS, 1 - Android
 
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 1) {
+        } else  {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Неизвестная операционная система");
         }
 
 // task 2
@@ -23,21 +21,17 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOS2 == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS2 == 1 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
-            System.out.println("Неизвестная операционная система");
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
 
 // task 3
         int year = 2024;
 
-        if (year > 1584) {
-            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-                System.out.println(year + " год является високосным");
-            } else {
-                System.out.println(year + " год не является високосным");
-            }
+        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+            System.out.println(year + " год является високосным");
+        } else if (year > 1584) {
+            System.out.println(year + " год не является високосным");
         } else {
             System.out.println("Год должен быть больше 1584");
         }
@@ -53,8 +47,8 @@ public class Main {
         } else if (deliveryDistance <= 100) {
             deliveryDays = 3;
         } else {
-            System.out.println("Доставка невозможна: расстояние свыше 100 км");
             deliveryDays = -1;
+            System.out.println("Доставка невозможна: расстояние свыше 100 км");
         }
 
         if (deliveryDays > 0) {
@@ -64,35 +58,33 @@ public class Main {
 //task 5
         int monthNumber = 12;
 
-        if (monthNumber < 1 || monthNumber > 12) {
-            System.out.println("Некорректный номер месяца. Введите число от 1 до 12");
-        } else {
-            switch (monthNumber) {
-                case 12:
-                case 1:
-                case 2:
-                    System.out.println("Месяц " + monthNumber + " принадлежит к сезону зима");
-                    break;
-                case 3:
-                case 4:
-                case 5:
-                    System.out.println("Месяц " + monthNumber + " принадлежит к сезону весна");
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                    System.out.println("Месяц " + monthNumber + " принадлежит к сезону лето");
-                    break;
-                case 9:
-                case 10:
-                case 11:
-                    System.out.println("Месяц " + monthNumber + " принадлежит к сезону осень");
-                    break;
-                default:
-                    System.out.println("Неизвестный месяц");
-                    break;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Месяц " + monthNumber + " принадлежит к сезону зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Месяц " + monthNumber + " принадлежит к сезону весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Месяц " + monthNumber + " принадлежит к сезону лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Месяц " + monthNumber + " принадлежит к сезону осень");
+                break;
+            default:
+                System.out.println("Некорректный номер месяца.");
+                break;
+
             }
         }
     }
-}
+
 
