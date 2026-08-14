2e63f3069e66119ce0cecfb15c8c20cbb2560927
KaSaXa <akurmelevv@gmail.com>
2026-08-14 17:16:07 +0300

    dasLastCompleted

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,10 @@ public class Main {
 
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else  {
+        } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Неизвестная ошибка");
         }
 
 // task 2
@@ -21,37 +23,51 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOS2 == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
+        } else if (clientOS2 == 1 && clientDeviceYear < 2015){
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Неизвестная система");
         }
 
 // task 3
-        int year = 2024;
-
-        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
-            System.out.println(year + " год является високосным");
-        } else if (year > 1584) {
-            System.out.println(year + " год не является високосным");
-        } else {
+        int year = 2021;
+        if (year < 1584) {
             System.out.println("Год должен быть больше 1584");
+            return;
+        }
+
+        boolean leapYear;
+
+        if (year % 400 == 0) {
+            leapYear = true;
+        } else if (year % 100 == 0) {
+            leapYear = false;
+        } else if (year % 4 == 0) {
+            leapYear = true;
+        } else {
+            leapYear = false;
+        }
+        if (leapYear) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
         }
 
 // task 4
         int deliveryDistance = 95;
         int deliveryDays = 0;
 
-        if (deliveryDistance <= 20) {
+        if (deliveryDistance <= 20 && deliveryDistance > 0) {
             deliveryDays = 1;
-        } else if (deliveryDistance <= 60) {
+        } else if (deliveryDistance <= 60 && deliveryDistance > 20) {
             deliveryDays = 2;
-        } else if (deliveryDistance <= 100) {
+        } else if (deliveryDistance <= 100 && deliveryDistance > 60) {
             deliveryDays = 3;
-        } else {
-            deliveryDays = -1;
+        } else if (deliveryDistance > 100){
             System.out.println("Доставка невозможна: расстояние свыше 100 км");
         }
 
-        if (deliveryDays > 0) {
+        if  (deliveryDays > 0) {
             System.out.println("Потребуется дней: " + deliveryDays);
         }
 
@@ -62,22 +78,22 @@ public class Main {
             case 1:
             case 2:
             case 12:
-                System.out.println("Месяц " + monthNumber + " принадлежит к сезону зима");
+                System.out.println(monthNumber + "-й месяц принадлежит к сезону зима");
                 break;
             case 3:
             case 4:
             case 5:
-                System.out.println("Месяц " + monthNumber + " принадлежит к сезону весна");
+                System.out.println(monthNumber + "-й месяц принадлежит к сезону весна");
                 break;
             case 6:
             case 7:
             case 8:
-                System.out.println("Месяц " + monthNumber + " принадлежит к сезону лето");
+                System.out.println(monthNumber + "-й месяц принадлежит к сезону лето");
                 break;
             case 9:
             case 10:
             case 11:
-                System.out.println("Месяц " + monthNumber + " принадлежит к сезону осень");
+                System.out.println(monthNumber + "-й месяц принадлежит к сезону осень");
                 break;
             default:
                 System.out.println("Некорректный номер месяца.");
